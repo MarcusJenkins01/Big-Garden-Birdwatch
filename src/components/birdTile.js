63dd8c03c5a4ca453a05cea133b3f9c6647f00eb
GGS92carmnad <azk20heu@uea.ac.uk>
2023-11-29 15:08:55 +0000

    Added the ability to view male and female birds

--- src/components/birdTile.js
+++ src/components/birdTile.js
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import './birdTile.css';
 
 function BirdTile(props) {
+  var bird = props.bird;
+  var hasSex = bird.imageMale && bird.imageFemale;
+  const [sex, setSex] = useState("m");
+  var imageSrc = hasSex ? (sex === "f" ? bird.imageFemale : bird.imageMale) : bird.image;
+
   return (
-    <div className="birdTile" onClick={props.onClick}>
+    <div className="birdTile">
       <div className="tileShadowContainer">
-        <div className="birdThumbnailContainer">
-          <img className="birdThumbnail" src={props.imageUrl}/>
+        { hasSex ?
+          <div className="sexButtons">
+            <button className={sex === "m" ? "selected" : ""} onClick={() => setSex("m")}>M</button>
+            <button className={sex === "f" ? "selected" : ""} onClick={() => setSex("f")}>F</button>
+          </div>
+          : <div className="sexButtons">
+            <button className="greyed">M</button>
+          </div>
+        }
+        <div className="birdThumbnailContainer" onClick={props.onClick}>
+          <img className="birdThumbnail" src={imageSrc}/>
         </div>
-        <div className="birdContent">
-          <p>{props.name}</p>
-          <div className="birdCount">{props.count}</div>
+        <div className="birdContent" onClick={props.onClick}>
+          <p>{bird.displayName}</p>
+          <div className="birdCount">{bird.count}</div>
         </div>
       </div>
     </div>
--- src/views/mainPage.js
+++ src/views/mainPage.js
@@ -7,26 +7,70 @@ import CountPopup from '../components/countPopup';
 
 function MainPage() {
   const [birds, setBirds] = useState({
-    blackbird: { displayName: "Blackbird", imageUrl: "bird_images/blackbird.jpg", count: 0 },
-    blue_tit: { displayName: "Blue tit", imageUrl: "bird_images/blue_tit.jpg", count: 0 },
-    carrion_crow: { displayName: "Carrion crow", imageUrl: "bird_images/carrion_crow.jpg", count: 0 },
-    chaffinch: { displayName: "Chaffinch", imageUrl: "bird_images/chaffinch.jpg", count: 0 },
-    coal_tit: { displayName: "Coal tit", imageUrl: "bird_images/coal_tit.jpg", count: 0 },
-    collared_dove: { displayName: "Collared dove", imageUrl: "bird_images/collared_dove.jpg", count: 0 },
-    dunnock: { displayName: "Dunnock", imageUrl: "bird_images/dunnock.jpg", count: 0 },
-    feral_pigeon: { displayName: "Feral pigeon", imageUrl: "bird_images/feral_pigeon.jpg", count: 0 },
-    goldfinch: { displayName: "Goldfinch", imageUrl: "bird_images/goldfinch.jpg", count: 0 },
-    great_tit: { displayName: "Great tit", imageUrl: "bird_images/great_tit.jpg", count: 0 },
-    greenfinch: { displayName: "Greenfinch", imageUrl: "bird_images/greenfinch.jpg", count: 0 },
-    house_sparrow: { displayName: "House sparrow", imageUrl: "bird_images/house_sparrow.jpg", count: 0 },
-    jackdaw: { displayName: "Jackdaw", imageUrl: "bird_images/jackdaw.jpg", count: 0 },
-    long_tailed_tit: { displayName: "Long-tailed tit", imageUrl: "bird_images/long_tailed_tit.jpg", count: 0 },
-    magpie: { displayName: "Magpie", imageUrl: "bird_images/magpie.jpg", count: 0 },
-    robin: { displayName: "Robin", imageUrl: "bird_images/robin.jpg", count: 0 },
-    rook: { displayName: "Rook", imageUrl: "bird_images/rook.jpg", count: 0 },
-    starling: { displayName: "Starling", imageUrl: "bird_images/starling.jpg", count: 0 },
-    woodpigeon: { displayName: "Wood pigeon", imageUrl: "bird_images/woodpigeon.jpg", count: 0 },
-    wren: { displayName: "Wren", imageUrl: "bird_images/wren.jpg", count: 0 },
+    blackbird: { displayName: "Blackbird", count: 0,
+    imageMale: "bird_images/new/blackbird_male.webp",
+    imageFemale: "bird_images/new/blackbird_female.webp" },
+
+    blue_tit: { displayName: "Blue tit", count: 0,
+    image: "bird_images/new/blue_tit.webp" },
+    
+    carrion_crow: { displayName: "Carrion crow", count: 0,
+    image: "bird_images/new/carrion_crow.webp" },
+
+    chaffinch: { displayName: "Chaffinch", count: 0,
+    imageMale: "bird_images/new/chaffinch_male.webp",
+    imageFemale: "bird_images/new/chaffinch_female.webp" },
+
+    coal_tit: { displayName: "Coal tit", count: 0,
+    image: "bird_images/new/coal_tit.webp" },
+
+    collared_dove: { displayName: "Collared dove", count: 0,
+    image: "bird_images/new/collared_dove.webp" },
+
+    dunnock: { displayName: "Dunnock", count: 0,
+    image: "bird_images/new/dunnock.webp" },
+
+    feral_pigeon: { displayName: "Feral pigeon", count: 0,
+    image: "bird_images/new/feral_pigeon.webp" },
+
+    goldfinch: { displayName: "Goldfinch", count: 0,
+    image: "bird_images/new/goldfinch.webp" },
+
+    great_tit: { displayName: "Great tit", count: 0,
+    imageMale: "bird_images/new/great_tit_male.webp",
+    imageFemale: "bird_images/new/great_tit_female.webp" },
+
+    greenfinch: { displayName: "Greenfinch", count: 0,
+    imageMale: "bird_images/new/greenfinch_male.webp",
+    imageFemale: "bird_images/new/greenfinch_female.webp" },
+
+    house_sparrow: { displayName: "House sparrow", count: 0,
+    imageMale: "bird_images/new/house_sparrow_male.webp",
+    imageFemale: "bird_images/new/house_sparrow_female.webp" },
+
+    jackdaw: { displayName: "Jackdaw", count: 0,
+    image: "bird_images/new/jackdaw.webp" },
+
+    long_tailed_tit: { displayName: "Long-tailed tit", count: 0,
+    image: "bird_images/new/long_tailed_tit.webp" },
+
+    magpie: { displayName: "Magpie", count: 0,
+    image: "bird_images/new/magpie.webp" },
+
+    robin: { displayName: "Robin", count: 0,
+    image: "bird_images/new/robin.webp" },
+
+    rook: { displayName: "Rook", count: 0,
+    image: "bird_images/new/rook.webp" },
+
+    starling: { displayName: "Starling", count: 0,
+    image: "bird_images/new/starling.webp" },
+
+    woodpigeon: { displayName: "Wood pigeon", count: 0,
+    image: "bird_images/new/wood_pigeon.webp" },
+
+    wren: { displayName: "Wren", count: 0,
+    image: "bird_images/new/wren.webp" },
   });
   
   const [searchInput, setSearchInput] = useState("");
@@ -57,7 +101,7 @@ function MainPage() {
         </div>
         <div className="birdGrid">
           { Object.entries(filteredBirds).map( ([key, value]) =>
-          <BirdTile key={key} name={value.displayName} imageUrl={value.imageUrl} count={value.count} onClick={() => tilePressed(key)}/> ) }
+          <BirdTile key={key} bird={value} onClick={() => tilePressed(key)}/> ) }
         </div>
         <div className="controls">
           <input className="App-button-neutral" type="button" value="Quit"/>
