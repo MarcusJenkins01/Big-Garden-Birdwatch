09ab052e5e003ec36471cee0b4f891daa049c187
GGS92carmnad <azk20heu@uea.ac.uk>
2023-12-13 20:28:47 +0000

    Added email format validation

--- src/views/registerPage.js
+++ src/views/registerPage.js
@@ -68,6 +68,13 @@ function RegisterPage(props) {
       setEmailError("Please enter your email");
       errors = true;
     }
+    // Email validation regex from https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+    else if (!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+      setEmailError("Please enter a valid email");
+      errors = true;
+    }
+
+    
 
     if (postcodeText.length === 0) {
       setPostcodeError("Please enter your postcode");
